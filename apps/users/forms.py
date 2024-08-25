518498cd888923c3aa23a7bfa4839b1a823a520f
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-08-25 16:22:32 +0545

    login model

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -6,13 +6,16 @@ class LoginForm(forms.Form):
     email = forms.EmailField(max_length=254, label='Email')
     password = forms.CharField(widget=forms.PasswordInput)
 
-    class Meta:
-        fields = ['email', 'password']
-
     def clean(self):
-        if self.cleaned_data.get('email') and self.cleaned_data.get('password'):
-            user = authenticate(username=self.cleaned_data.get('email'), password=self.cleaned_data.get('password'))
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            user = authenticate(username=email, password=password)
             if user is None:
                 raise forms.ValidationError("Email or password did not match.")
-            if not user.is_active == 'Active' or not user.is_staff == True or not user.is_verified == True:
+            if not user.is_active or not user.is_staff or not user.is_verified:
                 raise forms.ValidationError('User is not active or you do not have permission.')
+            cleaned_data['user'] = user
+        return cleaned_data
