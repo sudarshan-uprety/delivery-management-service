4f1c671854da00b495fd24e0032f100c21f50b40
sudarshan-uprety <mail@sudarshan-uprety.com.np>
2025-04-10 20:39:18 -0700

    database postgres and update requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -97,11 +97,7 @@ AUTH_USER_MODEL = 'users.User'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {**env.db('DATABASE_CREDENTIALS'),
-    'CONN_MAX_AGE': env.int('CONN_MAX_AGE')
-    }
-}
+DATABASES = {'default': {**env.db('DATABASE_CREDENTIALS'), 'CONN_MAX_AGE': env.int('CONN_MAX_AGE')}}
 
 
 # Password validation
